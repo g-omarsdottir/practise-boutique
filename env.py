8c4a4978635147f2f73150a9544f2a29a4b590c6
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-06-16 19:53:38 +0000

    Add video

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51PPQbyP15WHQfBzAXJQeQ9Z1dCpVI2RQWySGIEsBeZMYUhwbMWPOwzYEQGLkhbMGpdbUpP4qmFj9rpvhF1xLxxy100F5zvPBDU"
+os.environ["STRIPE_SECRET_KEY"] = "sk_test_51PPQbyP15WHQfBzA78F5j4GNlvZRjwpNZSiqpioV3fHlIYBStMA8OPbTTR8OniC3B46edVzct13Vo54qr2ogIX0K00aMm0Jx8L"
+os.environ["STRIPE_WH_SECRET"] = "whsec_y8lQUMYHrO2zgbXm0qYBEl2jl5I3osMz"
+os.environ["DATABASE_URL"] = "postgres://u3wb2hmg5a1:eq28MBaMR63Z@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/shiny_sport_aroma_532749"
+os.environ["CLOUDINARY_URL"] = "cloudinary://945954555739541:945954555739541@deqkqler9"
+os.environ["SECRET_KEY"] = "iejoeijoeijroeirjiejr84954l566§$"
